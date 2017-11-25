ca0f6d56f6d4ee98a6b50cfbdbf849a08a76dc12
Soon Van <cog@randomecho.com>
2017-11-25 11:58:14 -0500

    Remove date_modified from filename if present
    Fixes up previous run through where the date_modified was
    used as the marker instead of the expected date_taken EXIF data.

--- exifdater.rb
+++ exifdater.rb
@@ -14,6 +14,7 @@ require 'exifr'
 
 @found_count = 0
 @rename_count = 0
+@datemod_count = 0
 @skip_count = 0
 @error_count = 0
 
@@ -21,11 +22,33 @@ def valid_photo(filename)
   return File.extname(filename).downcase.match(/\.jpe?g/)
 end
 
+def remove_date_modified(photo, photo_dir)
+  photo_base = photo_dir + File::SEPARATOR
+  photo_file = photo_base + photo
+  photo_modified = EXIFR::JPEG.new(photo_file).date_time
+  photo_taken = EXIFR::JPEG.new(photo_file).date_time_original
+
+  if !photo_modified.nil? && photo_taken != photo_modified
+    datetime_modified = photo_modified.strftime("%Y%m%d_%H%M%S")
+
+    if File.fnmatch('*' + datetime_modified + '*', photo)
+      filename_without_date_modified = photo_file.sub(datetime_modified, '')
+      filename_without_date_modified.sub!('--', '-')
+
+      begin
+        File.rename(photo_file, filename_without_date_modified)
+        @datemod_count += 1
+      rescue
+        @error_count += 1
+      end
+    end
+  end
+end
+
 def rename_photo(photo, photo_dir)
   photo_base = photo_dir + File::SEPARATOR
   photo_file = photo_base + photo
   photo_taken = EXIFR::JPEG.new(photo_file).date_time_original
-  photo_modified = EXIFR::JPEG.new(photo_file).date_time
 
   unless photo_taken.nil?
     photo_stamp = photo_taken.strftime("%Y%m%d_%H%M%S")
@@ -37,7 +60,7 @@ def rename_photo(photo, photo_dir)
         File.rename(photo_file, photo_base + photo_stamp + '-' + photo)
         @rename_count += 1
       rescue
-      @error_count += 1
+        @error_count += 1
       end
     end
   else
@@ -50,7 +73,9 @@ def exifdater(photo_dir)
     Dir.foreach(photo_dir) do |photo|
       if valid_photo(photo)
         @found_count += 1
+
         rename_photo(photo, photo_dir)
+        remove_date_modified(photo, photo_dir)
       end
     end
   end
@@ -69,6 +94,10 @@ else
   
   if @skip_count > 0
     puts @skip_count.to_s + " skipped, date already in filename"
+  end
+
+  if @datemod_count > 0
+    puts @datemod_count.to_s + " renamed to remove date_modified from filename"
   end
 
   if @error_count > 0
